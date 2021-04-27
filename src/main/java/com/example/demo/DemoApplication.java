14a03f6a8deec94ee8b8ec79c1d5d2f876c03562
Harry Etherington <43179139+HenryEtherington@users.noreply.github.com>
2021-04-27 20:52:16 +0100

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,5 +9,10 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
+ @RestController
+  class HelloController {
+	@GetMapping(*/") 
+    String hello() {		
+        return "This tut";
+  }
 }
